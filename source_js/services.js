46713c069ebbbb07df7f0d02edc71b8969bff7e3
Andre Gou <andrejgou@gmail.com>
2016-12-05 15:50:15 -0600

    makes actual request to api

--- source_js/services.js
+++ source_js/services.js
@@ -2,34 +2,29 @@ var PPServices = angular.module('PPServices', []);
 
 PPServices.factory('CommonData', function() {
   var username = "";
+  var room = {};
   return {
     getUsername: function() {
       return username;
     },
     setUsername: function(newUsername) {
       username = newUsername;
+    },
+    getRoom: function() {
+      return room;
+    },
+    setRoom: function(newRoom) {
+      room = newRoom;
     }
   }
 });
 
 PPServices.factory('Backend', function($http, $window) {
-  var baseUrl = "";
-  var get = function() {
-    return $http.get(baseUrl);
-  }
+  var baseUrl = "/api";
+
   return {
     createRoom: function(roomname) {
-      var promise = new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          if (true) {
-            resolve("dummyId" + roomname);
-          } else {
-            reject(Error("It broke"));
-          }
-        }, 1000);
-      });
-
-      return promise;
+      return $http.post(baseUrl + "/room", {roomName: roomname});
     }
   }
 });
