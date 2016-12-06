dcd7c380e4582d762040f8eaf28ed9071347a316
Andre Gou <andrejgou@gmail.com>
2016-12-06 01:54:02 -0600

    added codemirror

--- source_js/app.js
+++ source_js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'PPControllers', 'PPServices']);
+var app = angular.module('app', ['ngRoute', 'PPControllers', 'PPServices', 'ui.codemirror']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
