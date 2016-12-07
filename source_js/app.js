83ff1b533a8c2a4aa93d48e35b914990dcc9c51d
Andre Gou <andrejgou@gmail.com>
2016-12-06 21:24:57 -0600

    messages scroll to bottom

--- source_js/app.js
+++ source_js/app.js
@@ -14,3 +14,18 @@ app.config(['$routeProvider', function($routeProvider) {
       redirectTo: '/landing'
     });
 }]);
+
+app.directive('scrollBottom', function() {
+  return {
+    scope: {
+      scrollBottom: "="
+    },
+    link: function(scope, element) {
+      scope.$watchCollection('scrollBottom', function(newValue) {
+        if (newValue) {
+          $(element).scrollTop($(element)[0].scrollHeight);
+        }
+      });
+    }
+  }
+})
